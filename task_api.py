31a13589c380a2fef6c9eb3fc14c5f44c7713bd2
Hms1000 <hmstakai@gmail.com>
2026-08-17 14:04:51 +0200

    added a full CRUD endpoints in fastapi

--- task_api.py
+++ task_api.py
@@ -2,6 +2,13 @@ from fastapi import FastAPI
 from pydantic import BaseModel
 import sqlite3
 
+class TaskIn(BaseModel):
+     title: str
+     priority: str
+     done: bool = False
+     due_date: str | None = None
+     frequency: str | None = None
+
 app = FastAPI() #fastapi client
 
 def get_connection(): #getting db connection and creating a cursor
@@ -36,3 +43,56 @@ def list_tasks():
       conn.close()
 
       return [dict(row) for row in rows] #converting sqlite3.Row into a real Python dict
+
+@app.post("/tasks")
+def create_task(task: TaskIn):
+     conn = get_connection()
+     cursor = conn.cursor()
+
+     cursor.execute(
+          "INSERT INTO tasks (title, priority, done, due_date, frequency) VALUES (?, ?, ?, ?, ?)",
+          (task.title, task.priority, task.done, task.due_date, task.frequency)
+                    )
+     conn.commit()
+     conn.close()
+
+     return {"message": f"Task '{task.title}' added"}
+
+@app.delete("/tasks/{title}")
+def delete_task(title: str):
+     conn = get_connection()
+     cursor = conn.cursor()
+
+     cursor.execute(
+          "DELETE FROM tasks WHERE title = ?",
+          (title,)
+     )
+
+     conn.commit()
+     conn.close()
+
+     return {"message": f"Deleted {title}"}
+
+@app.get("/tasks/filter")
+def filter_tasks(priority: str):
+     conn = get_connection()
+     cursor = conn.cursor()
+     cursor.execute("SELECT * FROM tasks WHERE priority = ?", (priority,))
+     rows = cursor.fetchall()
+     conn.close()
+
+     return [dict(row) for row in rows]
+
+@app.put("/tasks/{title}")
+def update_task(title: str, task:TaskIn):
+     conn = get_connection()
+     cursor = conn.cursor()
+
+     cursor.execute(
+          "UPDATE tasks SET priority = ?, done = ?, due_date = ?, frequency = ? WHERE title = ?",
+          (task.priority, task.done, task.due_date, task.frequency, title)
+     )
+     conn.commit()
+     conn.close()
+
+     return {"message": f"Updated {title}"}
