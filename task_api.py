ba0e262334bdd30bb17e8490a169068d5344959d
Hms1000 <hmstakai@gmail.com>
2026-08-19 09:16:15 +0200

    updated 'task_api.py' to use sqlmodel

--- task_api.py
+++ task_api.py
@@ -1,98 +1,60 @@
 from fastapi import FastAPI
-from pydantic import BaseModel
-import sqlite3
-
-class TaskIn(BaseModel):
-     title: str
+from sqlmodel import SQLModel, Field, create_engine, Session, select
+class Task(SQLModel, table=True):
+     id: int | None = Field(default=None, primary_key=True)
+     title: str = Field(unique=True)
      priority: str
      done: bool = False
      due_date: str | None = None
      frequency: str | None = None
 
+engine = create_engine("sqlite:///tracker.db")
+SQLModel.metadata.create_all(engine)
+
 app = FastAPI() #fastapi client
-
-def get_connection(): #getting db connection and creating a cursor
-    conn = sqlite3.connect("tracker.db")
-    conn.row_factory = sqlite3.Row
-    return conn
-
-setup_conn = get_connection()
-setup_cursor = setup_conn.cursor()
-
-
-# creating tasks table
-setup_cursor.execute("""
-    CREATE TABLE IF NOT EXISTS tasks (
-    title TEXT UNIQUE,
-    priority TEXT,
-    done BOOLEAN,
-    due_date TEXT,
-    frequency TEXT
-    )
-""")
-
-setup_conn.commit()
-setup_conn.close()
 
 @app.get("/tasks") # get endpoint used to list all tasks
 def list_tasks():
-      conn = get_connection()
-      cursor = conn.cursor()
-      cursor.execute("SELECT * FROM tasks")
-      rows = cursor.fetchall()
-      conn.close()
-
-      return [dict(row) for row in rows] #converting sqlite3.Row into a real Python dict
+      with Session(engine) as session:
+           return session.exec(select(Task)).all()
 
 @app.post("/tasks")
-def create_task(task: TaskIn):
-     conn = get_connection()
-     cursor = conn.cursor()
-
-     cursor.execute(
-          "INSERT INTO tasks (title, priority, done, due_date, frequency) VALUES (?, ?, ?, ?, ?)",
-          (task.title, task.priority, task.done, task.due_date, task.frequency)
-                    )
-     conn.commit()
-     conn.close()
-
-     return {"message": f"Task '{task.title}' added"}
+def create_task(task: Task):
+     with Session(engine) as session:
+          session.add(task)
+          session.commit()
+          session.refresh(task)
+          return task
+    
 
 @app.delete("/tasks/{title}")
 def delete_task(title: str):
-     conn = get_connection()
-     cursor = conn.cursor()
-
-     cursor.execute(
-          "DELETE FROM tasks WHERE title = ?",
-          (title,)
-     )
-
-     conn.commit()
-     conn.close()
-
-     return {"message": f"Deleted {title}"}
+     with Session(engine) as session:
+          statement = select(Task).where(Task.title == title)
+          task = session.exec(statement).first()
+          if not task:
+               return {"message": "Task not found"}
+          session.delete(task)
+          session.commit()
+          return {"message": f"Deleted {title}"}
 
 @app.get("/tasks/filter")
 def filter_tasks(priority: str):
-     conn = get_connection()
-     cursor = conn.cursor()
-     cursor.execute("SELECT * FROM tasks WHERE priority = ?", (priority,))
-     rows = cursor.fetchall()
-     conn.close()
-
-     return [dict(row) for row in rows]
+     with Session(engine) as session:
+          statement = select(Task).where(Task.priority == priority)
+          return session.exec(statement).all()
 
 @app.put("/tasks/{title}")
-def update_task(title: str, task:TaskIn):
-     conn = get_connection()
-     cursor = conn.cursor()
-
-     cursor.execute(
-          "UPDATE tasks SET priority = ?, done = ?, due_date = ?, frequency = ? WHERE title = ?",
-          (task.priority, task.done, task.due_date, task.frequency, title)
-     )
-     conn.commit()
-     conn.close()
-
-     return {"message": f"Updated {title}"}
+def update_task(title: str, updated:Task):
+     with Session(engine) as session:
+          statement = select(Task).where(Task.title == title)
+          task = session.exec(statement).first()
+          if not task:
+               return {"message": "Task not found"}
+          task.priority = updated.priority
+          task.done = updated.done
+          task.due_date = updated.due_date
+          task.frequency = updated.frequency
+          session.add(task)
+          session.commit()
+          return task
